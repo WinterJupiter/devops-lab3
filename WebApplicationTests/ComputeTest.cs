ffbbd7d568d250bd3aaea153da4a389adeae9a2d
Jupiter <90478585+WinterJupiter@users.noreply.github.com>
2024-04-27 12:51:04 +0400

    Update ComputeTest.cs

--- WebApplicationTests/ComputeTest.cs
+++ WebApplicationTests/ComputeTest.cs
@@ -25,5 +25,16 @@ namespace WebApplicationTests
 
             Assert.Equal(expectedMeanArea, meanArea, 1e-4);
         }
+
+        [Fact]
+        public void ComputeMinAreaTest3()
+        {
+            const double expectedMeanArea = 30.4167;
+
+            var areas = new List<double> { 2.5, 10, 15, 25, 30, 100 };
+            var meanArea = Utils.ComputeMeanArea(areas);
+
+            Assert.Equal(expectedMeanArea, meanArea, 1e-4);
+        }
     }
 }
